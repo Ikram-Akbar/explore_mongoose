551a44c92a743f8b7106b1112771b8d9d81ccd7c
Ikram-Git-Coder <ikramakbar71@gmail.com>
2023-05-16 03:22:39 +0600

    refactoring the file

--- src/app/Module/student/student.service.ts
+++ src/app/Module/student/student.service.ts
@@ -1,28 +1,22 @@
 import { IStudentInfo } from "./student.interface";
 import studentModel from "./student.model";
 
+
 export const createDataForDb = async (payload: IStudentInfo): Promise<IStudentInfo> => {
   const studentData = new studentModel(payload)
- /* const studentData = new studentModel({
-    id: "10",
-    role: "student",
-    name: {
-      fName: "ikram akbar",
-      mName: "Ikram ",
-      lName: "akbar",
-    },
-    gender: "male",
-    address: "Cumilla Chittagong Bangladesh",
-    contactNum: "01800000",
-    email: "test@ikram.akbar",
-    dateOfBirth: "01 march 1990",
-  });*/
   await studentData.save();
-  // console.log(studentData);
   return studentData;
 };
 
-export const getStudentsDataFromDb = async () => {
+
+/**
+ * 
+ * This is service 
+ * get all the data from the DB 
+ * Return Promise< IStudent[]> 
+ */
+
+export const getStudentsDataFromDb = async ():Promise<IStudentInfo[]> => {
   const data = await studentModel.find();
   return data;
 };
